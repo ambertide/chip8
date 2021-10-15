5cc84f0265ede20f7d0d1be576818eaf97510475
ambertide <egeemirozkan24@gmail.com>
2021-10-16 02:00:01 +0300

    Add sound support

--- chip8/emulator/sound.go
+++ chip8/emulator/sound.go
@@ -1,19 +1,23 @@
 package emulator
 
 import (
+	"time"
+
+	"github.com/faiface/beep"
 	"github.com/faiface/beep/generators"
 	"github.com/faiface/beep/speaker"
 )
 
 func BeepRoutine(soundTimer *bool) {
-	speaker.Init(240, 0)
-	sound, err := generators.SinTone(240, 119)
+	speaker.Init(44100, 735)
+	sound, err := generators.SinTone(44100, 1190)
 	if err != nil {
 		return
 	}
 	for {
 		if *soundTimer {
-			speaker.Play(sound)
+			speaker.Play(beep.Take(44100/60, sound))
 		}
+		time.Sleep(time.Second / 60)
 	}
 }
